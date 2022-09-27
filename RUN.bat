e1e92e8d508be7a741021ba5583578323caf33f0
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-27 12:53:22 -0400

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -2,8 +2,6 @@
 color 0a
 set /p Input=Enter Text (one word only for now): 
 python image-from-text.py --text=%Input% --no-mega
-echo upscale?
-pause
 python upscale.py 2 generated.png bilinear
 del generated.png
 echo upscaled!
